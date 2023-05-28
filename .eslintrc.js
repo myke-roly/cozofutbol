4469f41f78d1206bc48184d237b3b43ec6ba728b
mikebds <rchallapa@bancodelsol.com>
2023-05-27 23:53:48 -0300

    config reat hook error on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  semi: false,
-};
+  plugins: ['react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    semi: 0,
+  },
+}
