a22546a87b2f739fcbdbd360bd7beb71e4007da2
mikebds <rchallapa@bancodelsol.com>
2024-01-13 19:32:36 -0300

    test: Add @testing-library/react-native package and update ContentWithButtonsLayout test

--- src/components/layout/ContentWithButtonsLayout.test.tsx
+++ src/components/layout/ContentWithButtonsLayout.test.tsx
@@ -1,10 +1,102 @@
-import rendered from 'react-test-renderer'
+import renderer from 'react-test-renderer'
 import { ContentWithButtonsLayout } from './'
+import { View } from 'react-native'
+import { Text } from '../common'
 
 describe('ContentWithButtonsLayout', () => {
-  it('should render correctly', () => {
-    const tree = rendered.create(<ContentWithButtonsLayout />).toJSON()
+  const fn = jest.fn()
 
+  it('should render correctly', () => {
+    const tree = renderer.create(<ContentWithButtonsLayout />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should render correctly with children', () => {
+    const tree = renderer
+      .create(
+        <ContentWithButtonsLayout>
+          <View>
+            <Text>Child Content 1</Text>
+          </View>
+        </ContentWithButtonsLayout>,
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should render correctly with primary button', () => {
+    const tree = renderer
+      .create(
+        <ContentWithButtonsLayout
+          primaryButton={{
+            title: 'primary-title-test',
+            onPress: fn,
+          }}
+        />,
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should render correctly with secondary button', () => {
+    const tree = renderer
+      .create(
+        <ContentWithButtonsLayout
+          secondaryButton={{
+            title: 'secondary-title-test',
+            onPress: fn,
+          }}
+        />,
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should render correctly with primary and secondary button', () => {
+    const tree = renderer
+      .create(
+        <ContentWithButtonsLayout
+          primaryButton={{
+            title: 'primary-title-test',
+            onPress: fn,
+          }}
+          secondaryButton={{
+            title: 'secondary-title-test',
+            onPress: fn,
+          }}
+        />,
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Should render correctly with secondary button outline variant', () => {
+    const tree = renderer
+      .create(
+        <ContentWithButtonsLayout
+          primaryButton={{
+            title: 'primary-title-test',
+            onPress: fn,
+            variant: 'outline',
+          }}
+        />,
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Should render correctly if isfocued is true', () => {
+    const tree = renderer
+      .create(
+        <ContentWithButtonsLayout
+          primaryButton={{
+            title: 'primary-title-test',
+            onPress: fn,
+          }}
+          isfocued={true}
+        />,
+      )
+      .toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
