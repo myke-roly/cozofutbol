cfb107985e50a0e1f9b340c77a8fe4554f44cdeb
mikebds <rchallapa@bancodelsol.com>
2024-01-14 03:37:49 -0300

    test: Add KeyboardAvoidingComponent and its test file

---
+++ src/components/common/KeyboardAvoidingComponent.test.tsx
@@ -0,0 +1,41 @@
+import renderer from 'react-test-renderer'
+import { fireEvent, render } from '@testing-library/react-native'
+import { View } from 'react-native'
+import KeyboardAvoidingComponent from './KeyboardAvoidingComponent'
+import { Text } from './'
+
+describe('KeyboardAvoidingComponent', () => {
+  it('should render correctly', () => {
+    const tree = renderer
+      .create(
+        <KeyboardAvoidingComponent>
+          <View>
+            <Text>Children custom keyboard avoiding</Text>
+          </View>
+        </KeyboardAvoidingComponent>,
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Should render correctly sets height onLayout', () => {
+    const nativeEventMock = {
+      nativeEvent: {
+        layout: {
+          height: 100,
+        },
+      },
+    }
+
+    const { toJSON, getByTestId } = render(
+      <KeyboardAvoidingComponent>
+        <View>
+          <Text>Children custom keyboard avoiding</Text>
+        </View>
+      </KeyboardAvoidingComponent>,
+    )
+
+    fireEvent(getByTestId('keyboard-avoiding-view'), 'layout', nativeEventMock)
+    expect(toJSON()).toMatchSnapshot()
+  })
+})
--- src/components/common/KeyboardAvoidingComponent.tsx
+++ src/components/common/KeyboardAvoidingComponent.tsx
@@ -5,6 +5,7 @@ import {
   Dimensions,
   Platform,
   KeyboardAvoidingViewProps,
+  StyleSheet,
 } from 'react-native'
 
 const windowHeight = Dimensions.get('window').height
@@ -12,25 +13,36 @@ const windowHeight = Dimensions.get('window').height
 // TODO: CREATE DINAMIC SCROLL VIEW TO SET HEIGHT ON KEYBOARD OPEN AND CLOSE
 
 const KeyboardAvoidingComponent = ({
-  keyboardVerticalOffset,
+  keyboardVerticalOffset = 0,
+  children,
   ...props
 }: KeyboardAvoidingViewProps) => {
   const [verticalOffset, setVerticalOffset] = useState<number | undefined>(
     keyboardVerticalOffset,
   )
 
+  console.log('render KeyboardAvoidingComponent', verticalOffset)
+
   return (
     <View
+      style={styles.container}
+      testID="keyboard-avoiding-view"
       onLayout={({ nativeEvent: { layout } }) =>
         setVerticalOffset(windowHeight - layout.height)
       }>
       <KeyboardAvoidingView
+        style={styles.container}
         behavior={Platform.select({ ios: 'padding' })}
         keyboardVerticalOffset={verticalOffset}
-        {...props}
-      />
+        {...props}>
+        {children}
+      </KeyboardAvoidingView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+})
 
 export default KeyboardAvoidingComponent
